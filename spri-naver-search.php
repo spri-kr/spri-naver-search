3a978232453f0595ede63f7abbfada278fff5de4
Ungsik yun <ungsik.yun@gmail.com>
2015-07-10 11:27:56 +0900

    add query exist in db check code

--- spri-naver-search.php
+++ spri-naver-search.php
@@ -13,52 +13,60 @@ Description: Shortcode generating specific search result from naver.
  * Key and query is required
  * */
 
-class spri_naver_search {
-	function __construct(){
-		add_shortcode('spri-naver-search', array($this, 'naver_search'));
-		register_activation_hook( __FILE__, array($this, 'activation' ));
+class spri_naver_news {
+	function __construct() {
+		add_shortcode( 'spri-naver-search', array( $this, 'naver_search' ) );
+		register_activation_hook( __FILE__, array( $this, 'activation' ) );
 	}
 
-	function naver_search($attr){
-		$attrs =  shortcode_atts( array(
-			'key' => 'c1b406b32dbbbbeee5f2a36ddc14067f', // dummy key
-			'query' => 'SPRI',
-			'target' => 'news',
-			'display' => '10',
-			'start' => '1',
-			'sort' => 'sim',
-			'class' => 'spri-naver-search',
+	function naver_search( $attr ) {
+		$attrs = shortcode_atts( array(
+			'key'      => 'c1b406b32dbbbbeee5f2a36ddc14067f', // dummy key
+			'query'    => 'SPRI',
+			'target'   => 'news',
+			'display'  => '10',
+			'start'    => '1',
+			'sort'     => 'sim',
+			'class'    => 'spri-naver-search',
 			'template' => 'basic',
 		), $attr );
 
+		//Get xml content
 		$url = "http://openapi.naver.com/search?";
-		$url .= http_build_query($attrs);
+		$url .= http_build_query( $attrs );
 
-		$data =file_get_contents($url);
-		$xml = simplexml_load_string($data);
+		$data = file_get_contents( $url );
+		$xml  = simplexml_load_string( $data );
+
+		//check if search is first time
+		if ( $this->is_exist_query( $attrs->query ) ) {
+
+
+		}
+
 
 		$temp = array();
 
-		foreach($xml->channel->item as $data) {
+		foreach ( $xml->channel->item as $data ) {
 			$temp[] = $data;
 		}
-		usort($temp, function($a, $b){
-			$a_date = date_create_from_format('D, d M Y H:i:s T', $a->pubDate);
-			$b_date = date_create_from_format('D, d M Y H:i:s T', $b->pubDate);
+		usort( $temp, function ( $a, $b ) {
+			$a_date = date_create_from_format( 'D, d M Y H:i:s T', $a->pubDate );
+			$b_date = date_create_from_format( 'D, d M Y H:i:s T', $b->pubDate );
 
 			return $a_date < $b_date;
-		});
+		} );
 
 		$html = "<div class='$attrs[class]' >";
 
-		foreach($temp as $data){
-			$title = (string)$data->title;
-			$link = (string)$data->link;
-			$originallink = (string)$data->originallink;
-			$description = (string)$data->description;
-			$pubDate = date_create_from_format('D, d M Y H:i:s T', $data->pubDate);
+		foreach ( $temp as $data ) {
+			$title        = (string) $data->title;
+			$link         = (string) $data->link;
+			$originallink = (string) $data->originallink;
+			$description  = (string) $data->description;
+			$pubDate      = date_create_from_format( 'D, d M Y H:i:s T', $data->pubDate );
 
-			require (plugin_dir_path(__FILE__)."template/".$attrs['template'].".php");
+			require( plugin_dir_path( __FILE__ ) . "template/" . $attrs['template'] . ".php" );
 
 		}
 
@@ -67,25 +75,60 @@ class spri_naver_search {
 		return $html;
 	}
 
-	function activation(){
+	function activation() {
 		global $wpdb;
 		$charset_collate = $wpdb->get_charset_collate();
 
-		$table_name      = $wpdb->prefix . "spri_naver_search";
+		$article_table = $wpdb->prefix . "spri_naver_news_article";
+		$query_table   = $wpdb->prefix . "spri_naver_news_query";
 
-		$sql = "CREATE TABLE $table_name (
+		$sql1 = "CREATE TABLE $article_table (
 				id INT(11) NOT NULL AUTO_INCREMENT,
+				query_id int(11) NOT NULL,
 				title VARCHAR(50) NOT NULL,
-
+				title_hash BINARY(20) NOT NULL,
+				originallink VARCHAR(512),
+				link VARCHAR(512) NOT NULL,
+				description VARCHAR(512) NOT NULL,
+				pubDate DATE NOT NULL,
 				PRIMARY KEY (id),
+				index(query_id, pubDate),
+				index(query_id),
+				index(title),
+				index(title_hash),
+				index(pubDate)
+				) $charset_collate;
+				";
+
+		$sql2 = "CREATE TABLE $query_table (
+				id int(11) NOT NULL AUTO_INCREMENT,
+				query VARCHAR(100) NOT NULL,
+				query_hash char(40) NOT NULL,
+				PRIMARY KEY (id),
+				index (id, query_hash),
+				index (query_hash),
+				index (query),
+				index (id)
+
 				) $charset_collate;
 				";
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		dbDelta( $sql );
+		dbDelta( $sql1 );
+		dbDelta( $sql2 );
+	}
+
+	function is_exist_query( $q ) {
+		global $wpdb;
+
+		$q_hash = hexdec( sha1( $q ) );
+		$sql = "select exists(select * from wp_spri_naver_news_query where query_hash = 'f71e27b99793c8ae57803ae4a55d7a0ff390e260') AS exist";
+		$r = $wpdb->get_row($sql);
+
+		return $r->exist;
+		//$wpdb->get
 	}
 }
 
-new spri_naver_search();
+new spri_naver_news();
 
 //register_activation_hook( __FILE__, 'spri_chart_create_table' );
-
