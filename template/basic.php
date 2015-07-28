d363fe2aea87dbf6f461d649f82ddbc4a5b5ce1e
Ungsik yun <ungsik.yun@gmail.com>
2015-07-28 17:23:37 +0900

    modify template

--- template/basic.php
+++ template/basic.php
@@ -2,9 +2,9 @@
 $template = <<< ARTICLE
 
 <div class='item' >
-<h4 class='title'><a href='{$originallink}' target='_blank'> {$title}</a></h4>
-<p class='description'> {$description} </p>
-<p class='pubdate'>{$pubDate}</p>
+<h4 class='title'><a href='{$item->originallink}' target='_blank'> {$item->title}</a></h4>
+<p class='description'> {$item->description} </p>
+<p class='pubdate'>{$item->pubDate}</p>
 </div>
 
 ARTICLE;
