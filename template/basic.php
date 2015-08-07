0f2369b8044a8b2f2907de7f0854b88b39ed84d2
Ungsik yun <ungsik.yun@gmail.com>
2015-08-07 16:33:53 +0900

    update template do not link to broken link

--- template/basic.php
+++ template/basic.php
@@ -1,11 +1,20 @@
 <?php
 
 $temp_date = date('Y년 m월 d일',strtotime($item->pubDate));
+
+$href = "href='{$item->originallink}";
+
+if($item->originallink == $item->link)
+{
+	$href = "";
+	$target = "_self";
+}
+
 //$temp_date = $item->pubDate;
 $template = <<< ARTICLE
 
 <div class='item' >
-<h4 class='title'><a href='{$item->originallink}' target='_blank'> {$item->title}</a></h4>
+<h4 class='title'><a {$href} target='{$target}'> {$item->title}</a></h4>
 <p class='description'> {$item->description} </p>
 <p class='pubdate'>{$temp_date}</p>
 </div>
