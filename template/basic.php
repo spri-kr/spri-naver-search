046b0be02a451da0154b59408d6e44a9a4f0af48
Ungsik yun <ungsik.yun@gmail.com>
2015-08-19 15:29:23 +0900

    update template logic

--- template/basic.php
+++ template/basic.php
@@ -3,18 +3,18 @@
 $temp_date = date( 'Y년 m월 d일', strtotime( $item->pubDate ) );
 
 $href = "href='{$item->originallink}'";
-$target = "_blank";
+$target = "target='_blank'";
 
 if ( $item->originallink == $item->link ) {
 	$href = "";
-	$target = "_self";
+	$target = "target='_self";
 }
 
 //$temp_date = $item->pubDate;
 $template .= <<< ARTICLE
 
 <div class='item' >
-<h4 class='title'><a {$href} target='{$target}'> {$item->title}</a></h4>
+<h4 class='title'><a {$href} {$target}> {$item->title}</a></h4>
 <p class='description'> {$item->description} </p>
 <p class='pubdate'>{$temp_date}</p>
 </div>
